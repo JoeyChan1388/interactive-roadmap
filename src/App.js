5bd232c2d0e854fcc6141fa9c2fddd42519e461f
JoeyChan1388 <joey_chan1388@outlook.com>
2022-03-16 14:08:55 -0400

    Merge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Pages/Signin';
 import Home from './Components/Pages/Home';
+import About from './Components/Pages/About';
+import Contact from './Components/Pages/Contact';
 import Header from './Components/Header';
 import JSRoadmap from './Components/Pages/JSRoadmap';
 import HTMLRoadmap from './Components/Pages/HTMLRoadmap';
@@ -24,6 +26,8 @@ function App() {
           <Route exact path="/roadmaps/CSS" element={<CSSRoadmap />} />
           <Route exact path="/roadmaps/CSharp" element={<CSRoadmap />} />
           <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
           <Route path="/profile/:id" element={<Profile/>} />
         </Routes>
       </Router>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import './Header.css'
+import './Header.css';
 
 const Header = () => {
   return (
     <div className="header">
       <header>
-        <a href="/"><img class="logo" src="/logo.jpeg" alt="logo" /></a>
 
+        <a href="/" className="logo-img"><img class="logo" src="/logo.jpeg" alt="logo" /></a>
         <div class="navbar">
-          <a href="/coding"> Coding</a>
           <a href="/about"> About</a>
+          <a href="/roadmaps"> Roadmaps</a>
           <a href="/signup"> Register</a>
-          <a href="/help"> Help</a>
-          <a class="cta" href="/login"><button>Sign In</button></a>
+          <a href="/contact"> Contact Us</a>
+          <a class="cta" href="/signin"><button>Sign In</button></a>
         </div>
       </header>
 
